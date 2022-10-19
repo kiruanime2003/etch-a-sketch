6f1005d10fa4ee4653e3e9115016e29377526673
kiruanime2003 <kiruanime2003@gmail.com>
2022-10-19 21:51:00 +0530

    added drag event to grid

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-document.addEventListener("DOMContentLoaded", function(){
-    
+document.addEventListener("DOMContentLoaded", function () {
+
     var gridContainer = document.getElementById("grid-container");
 
-    for(let i=0; i<900; i++){
+    for (let i = 0; i < 900; i++) {
         let gridCell = document.createElement("div");
-        gridContainer.appendChild(gridCell).className="grid-cell";
+        gridContainer.appendChild(gridCell).className = "grid-cell";
     }
+
+    var gridCells = Array.from(document.getElementsByClassName("grid-cell"));
+    var mouseIsDown = false
+
+    for (let i = 0; i < 900; i++) {
+        gridCells[i].addEventListener('mousedown', function () { mouseIsDown = true });
+    }
+
+    for (let i = 0; i < 900; i++) {
+        gridCells[i].addEventListener('mouseup', function () { mouseIsDown = false });
+    }
+
+    for (let i = 0; i < 900; i++) {
+        gridCells[i].addEventListener('mousemove', function(){
+            if(mouseIsDown){
+                gridCells[i].style.backgroundColor = "#333";
+            }
+        })
+    }
+    
 });
