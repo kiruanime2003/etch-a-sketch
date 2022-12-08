00aac060b1ff7efbb113d5a7aebc9cecca25386d
kiruanime2003 <kiruanime2003@gmail.com>
2022-12-08 11:53:11 +0530

    created gridCreation func

--- app.js
+++ app.js
@@ -3,65 +3,76 @@ document.addEventListener("DOMContentLoaded", function () {
     var gridContainer = document.getElementById("grid-container");
     var inputColorValue = document.getElementById("brush-color");
     var buttonsList = Array.from(document.getElementsByClassName("buttons"));
-
+    
     let w = window.innerWidth;
+    var n;
 
-    var n=0;
-
-    if(w <= 1213) n=200;
-    else if(w)
-
-    for (let i = 0; i < n; i++) {
-        let gridCell = document.createElement("div");
-        gridContainer.appendChild(gridCell).className = "grid-cell";
+    if (w < 1213 && w > 935) {
+        n = 900;
+        console.log(n);
+        gridCreation(n);
     }
 
-    //color button
-    var gridCells = Array.from(document.getElementsByClassName("grid-cell"));
-    var mouseIsDown = false;
-
-    for (let i = 0; i < n; i++) {
-        gridCells[i].addEventListener('mousedown', function () { mouseIsDown = true });
+    else if (w < 935 && w > 800) {
+        n = 450;
+        console.log(n);
+        gridCreation(n);
     }
 
-    for (let i = 0; i < n; i++) {
-        gridCells[i].addEventListener('mouseup', function () { mouseIsDown = false });
-    }
+    function gridCreation(a) {
 
-    for (let i = 0; i < n; i++) {
-        gridCells[i].addEventListener('mousemove', function () {
-            if (mouseIsDown) {
-                gridCells[i].style.backgroundColor = inputColorValue.value;
-            }
-        })
-    }
+        for (let i = 0; i < a; i++) {
+            let gridCell = document.createElement("div");
+            gridContainer.appendChild(gridCell).className = "grid-cell";
+        }
 
-    //reinitializing color
-    inputColorValue.addEventListener("click", function () {
-        for (let i = 0; i < n; i++) {
+        var gridCells = Array.from(document.getElementsByClassName("grid-cell"));
+        var mouseIsDown = false;
+
+        //color button
+        for (let i = 0; i < a; i++) {
+            gridCells[i].addEventListener('mousedown', function () { mouseIsDown = true });
+        }
+
+        for (let i = 0; i < a; i++) {
+            gridCells[i].addEventListener('mouseup', function () { mouseIsDown = false });
+        }
+
+        for (let i = 0; i < a; i++) {
             gridCells[i].addEventListener('mousemove', function () {
                 if (mouseIsDown) {
                     gridCells[i].style.backgroundColor = inputColorValue.value;
                 }
             })
         }
-    });
 
-    //eraser button
-    buttonsList[0].addEventListener("click", function () {
-        for (let i = 0; i < n; i++) {
-            gridCells[i].addEventListener('mousemove', function () {
-                if (mouseIsDown) {
-                    gridCells[i].style.backgroundColor = "#fff";
-                }
-            })
-        }
-    });
+        //reinitializing color
+        inputColorValue.addEventListener("click", function () {
+            for (let i = 0; i <a; i++) {
+                gridCells[i].addEventListener('mousemove', function () {
+                    if (mouseIsDown) {
+                        gridCells[i].style.backgroundColor = inputColorValue.value;
+                    }
+                })
+            }
+        });
 
-    //clear button
-    buttonsList[1].addEventListener("click", function () {
-        for (let i = 0; i < n; i++) {
-            gridCells[i].style.backgroundColor = "#fff";
-        }
-    });
+        //eraser button
+        buttonsList[0].addEventListener("click", function () {
+            for (let i = 0; i < a; i++) {
+                gridCells[i].addEventListener('mousemove', function () {
+                    if (mouseIsDown) {
+                        gridCells[i].style.backgroundColor = "#fff";
+                    }
+                })
+            }
+        });
+
+        //clear button
+        buttonsList[1].addEventListener("click", function () {
+            for (let i = 0; i < a; i++) {
+                gridCells[i].style.backgroundColor = "#fff";
+            }
+        });
+    }
 });
